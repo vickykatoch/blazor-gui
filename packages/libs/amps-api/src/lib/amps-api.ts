7defd7608f218fc5dcb6c86ee3de5e05a53def6d
Balwinder Katoch <vickykatoch@yahoo.com>
2024-05-23 20:19:16 -0400

    Update amps-api.ts

--- packages/libs/amps-api/src/lib/amps-api.ts
+++ packages/libs/amps-api/src/lib/amps-api.ts
@@ -1,3 +1,13 @@
 export function ampsApi(): string {
   return 'amps-api';
 }
+
+
+export function replaceUrls(text: string): string {
+  const urlPattern =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  return text.replace(
+    urlPattern,
+    '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
+  );
+}
